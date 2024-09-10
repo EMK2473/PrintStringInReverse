79b2ddff3f02db30873ec60b4ed8280605b38b44
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-10 14:21:25 -0400

    declared and assigned variables; set up parent loop while scnr has next line; set index to origString length - 1; looped through index while gt0; build new string with reversed index; print reveresed string

--- PrintStringInReverse.java
+++ PrintStringInReverse.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class PrintStringInReverse {
    public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
     
+    while(scnr.hasNextLine()){
+        String origString = scnr.nextLine();
+        String reverseString = "";
+        int index = origString.length() - 1;
+
+        while(index >= 0) {
+            reverseString += origString.charAt(index);
+            index--;
+        }
+        System.out.println(reverseString);
+    }
+    scnr.close();
    }
 }
